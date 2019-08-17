c27757b2be3b0e9325f67c13dfeb0337f165bcfe
ahmad <ahmadkam1993@gmail.com>
2019-08-17 17:04:11 +0430

    init project

---
+++ src/containers/Scenes/HomePage/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const HomePage = () => (
+  <div className="homePage">
+    <div className="headerWrapper">
+      <h1>Header</h1>
+    </div>
+    <div className="bodyWrapper">
+      <h2>List Property</h2>
+    </div>
+    <div className="footerWrapper">
+      <h3>Footer</h3>
+    </div>
+  </div>
+);
+
+export default HomePage;
