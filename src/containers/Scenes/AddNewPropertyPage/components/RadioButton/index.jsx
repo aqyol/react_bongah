6d61ad050cd4f7831c16d47402fa479d0bb1917d
ahmad <ahmadkam1993@gmail.com>
2019-08-20 20:29:33 +0430

    Radio Button component edited

--- src/containers/Scenes/AddNewPropertyPage/components/RadioButton/index.jsx
+++ src/containers/Scenes/AddNewPropertyPage/components/RadioButton/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FaCircle } from 'react-icons/fa';
+import { FaCheck } from 'react-icons/fa';
 
 
 const RadioButton = props => (
   <div className="radio custom-radio">
     <label htmlFor={props.children}>
       <input id={props.children} type="radio" name="radio1" />
-      <FaCircle /> {props.children}
+      <span><FaCheck /></span> {props.children}
     </label>
   </div>
 );
