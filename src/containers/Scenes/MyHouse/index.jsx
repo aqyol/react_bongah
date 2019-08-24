59aabfdfb12194d98e0fc469c394c1db674f5493
ahmad <ahmadkam1993@gmail.com>
2019-08-24 20:39:39 +0430

    MyHouse component data edited

--- src/containers/Scenes/MyHouse/index.jsx
+++ src/containers/Scenes/MyHouse/index.jsx
@@ -5,11 +5,11 @@ import SingleHouse from '../../../shared/components/SingleHouse';
 
 
 const houseData = {
-  name: 'Modern Residence in New York',
-  address: ' 39 Remsen St, Brooklyn, NY 11201, USA',
-  beds: 3,
-  toilets: 2,
-  square: 20,
+  name: 'خانه ویلایی با استخر روباز',
+  address: 'تهران، شریعتی، خیابان عدالت شرقی، پلاک ۹۲۴',
+  beds: 6,
+  toilets: 10,
+  square: 500,
   img: 'http://mariusn.com/themes/reales/images/prop/1-1.png',
 };
 
@@ -17,7 +17,7 @@ const MyHouse = props => (
   <div className="myhousePage">
     <Dashboard>
       <div className="dashboardTitle">
-        <h3>Property ({props.match.params.id})</h3>
+        <h3>({props.match.params.id})</h3>
       </div>
       <div className="myHouseWrapper">
         <SingleHouse data={houseData} />
