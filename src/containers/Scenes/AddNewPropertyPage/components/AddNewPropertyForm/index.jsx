7e1bfd281738b26a7f9b14bc6a36aa3e0207ad6c
ahmad <ahmadkam1993@gmail.com>
2019-09-03 16:07:02 +0430

    loading spinner added

--- src/containers/Scenes/AddNewPropertyPage/components/AddNewPropertyForm/index.jsx
+++ src/containers/Scenes/AddNewPropertyPage/components/AddNewPropertyForm/index.jsx
@@ -972,6 +972,7 @@ class AddNewPropertyForm extends PureComponent {
       toggleTools: false,
       toggleOthers: false,
       toggleMap: false,
+      isLoading: false,
       moreInfo: {
         rooms: '',
         floors: '',
@@ -1146,6 +1147,10 @@ class AddNewPropertyForm extends PureComponent {
     console.group('save click');
     console.log(this.state);
     console.groupEnd();
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
   }
 
   render() {
@@ -1247,6 +1252,17 @@ class AddNewPropertyForm extends PureComponent {
 
     return (
       <div className="newPropertyForm">
+        {this.state.isLoading
+        && (
+          <div className={`load${this.state.isLoading ? '' : ' loaded'}`}>
+            <div className="load__icon-wrap">
+              <svg className="load__icon">
+                <path fill="#4ce1b6" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+              </svg>
+            </div>
+          </div>
+        )
+        }
         <Form>
           <Row form>
             <Col md={4} sm={6} xs={12}>
