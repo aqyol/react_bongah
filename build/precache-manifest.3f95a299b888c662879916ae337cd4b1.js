d657aadf7440438ae8750a092e0ef1c44dd6db57
ahmad <ahmadkam1993@gmail.com>
2019-09-22 19:07:38 +0330

    first build project

---
+++ build/precache-manifest.3f95a299b888c662879916ae337cd4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "b6de3c1a9121c14fd4ae",
+    "url": "/static/js/runtime~main.b6de3c1a.js"
+  },
+  {
+    "revision": "787c6384b676b2519cb9",
+    "url": "/static/js/main.787c6384.chunk.js"
+  },
+  {
+    "revision": "2fae4ae3529a170813d1",
+    "url": "/static/js/2.2fae4ae3.chunk.js"
+  },
+  {
+    "revision": "787c6384b676b2519cb9",
+    "url": "/static/css/main.d82cb356.chunk.css"
+  },
+  {
+    "revision": "2fae4ae3529a170813d1",
+    "url": "/static/css/2.975530af.chunk.css"
+  },
+  {
+    "revision": "755b7d9b9917b86fcb7b4dc49eebcfb6",
+    "url": "/index.html"
+  }
+]);
