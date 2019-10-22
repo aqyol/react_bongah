8499499a503b2a471aebfdda4af8dbbd70551a3f
ahmad <ahmadkam1993@gmail.com>
2019-10-22 16:28:02 +0330

    build project ans add cache control params to index.html file in build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51203f8f0e507269e7971eefbf2ba061.js"
+  "/precache-manifest.35c496cd1452bd34b9fb83543af06bdc.js"
 );
 
 self.addEventListener('message', (event) => {
