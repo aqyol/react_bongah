39e0fbb2a96042c83fb051aa2cd26c966f377f87
ahmad <ahmadkam1993@gmail.com>
2019-10-29 17:15:29 +0330

    report modal added to ads detail page,

--- src/containers/Scenes/AdsDetail/components/AdsDetailForm.jsx
+++ src/containers/Scenes/AdsDetail/components/AdsDetailForm.jsx
@@ -8,6 +8,11 @@ import {
   Label,
   Button,
   Collapse,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
 } from 'reactstrap';
 import CedarMaps from '@cedarstudios/react-cedarmaps';
 import { Line } from 'react-chartjs-2';
@@ -24,6 +29,7 @@ import {
   FaAngleUp,
   FaAngleDown,
 } from 'react-icons/fa';
+import renderSelectField from '../../../../shared/components/form/Select';
 
 
 const data = {
@@ -93,11 +99,48 @@ const {
   ScaleControl,
 } = CedarMaps.getReactMapboxGl();
 
+const reportTypes = [
+  {
+    value: '0',
+    label: 'کلاه برداری / دروغ',
+  },
+  {
+    value: '1',
+    label: 'کالای دزدی',
+  },
+  {
+    value: '2',
+    label: 'قیمت بیش از حد بازار',
+  },
+  {
+    value: '3',
+    label: 'محتوای نامناسب',
+  },
+  {
+    value: '4',
+    label: 'آگهی تکراری',
+  },
+  {
+    value: '4',
+    label: 'فروخته شده / ناموجود',
+  },
+  {
+    value: '4',
+    label: 'دسته بندی اشتباه',
+  },
+  {
+    value: '4',
+    label: 'سایر',
+  },
+];
+
 class AdsDetailForm extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: false,
+      reportModal: false,
+      reportType: '',
       adsInfo: {
         isOwner: false,
         type: '',
@@ -121,6 +164,11 @@ class AdsDetailForm extends PureComponent {
     this.handleEmail = this.handleEmail.bind(this);
     this.handleMessage = this.handleMessage.bind(this);
     this.toggle = this.toggle.bind(this);
+    this.handleToggleModal = this.handleToggleModal.bind(this);
+    this.handleDismissModal = this.handleDismissModal.bind(this);
+    this.isItemValid = this.isItemValid.bind(this);
+    this.handleSaveReport = this.handleSaveReport.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
 
   componentDidMount() {
@@ -176,6 +224,38 @@ class AdsDetailForm extends PureComponent {
     });
   }
 
+  handleSelect(name, value) {
+    this.setState({ [name]: value });
+  }
+
+  handleToggleModal(name) {
+    this.setState((prevState) => {
+      const { [name]: prevVal } = prevState;
+      return {
+        [name]: !prevVal,
+      };
+    });
+  }
+
+  handleDismissModal(name) {
+    this.setState({ [name]: false });
+  }
+
+  isItemValid(name) {
+    const { [name]: val } = this.state;
+    return val !== '';
+  }
+
+  handleSaveReport() {
+    this.setState({ reportModalLoading: true });
+    setTimeout(() => {
+      this.setState({
+        reportModalLoading: false,
+        reportModal: false,
+      });
+    }, 1000);
+  }
+
   render() {
     const info = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
       <Col lg={3} md={6} sm={4} xs={6} className="text-center" key={i.toString()}>
@@ -208,6 +288,71 @@ class AdsDetailForm extends PureComponent {
         {!this.state.isLoading
         && (
           <div className="ads-detail-container">
+            <Modal
+              fade={false}
+              isOpen={this.state.reportModal}
+              toggle={() => { this.handleToggleModal('reportModal'); }}
+            >
+              <ModalHeader>درخواست جدید</ModalHeader>
+              {this.state.reportModalLoading
+              && (
+                <div className={`relative-load${this.state.isLoading ? '' : ' loaded'}`}>
+                  <div className="load__icon-wrap">
+                    <svg className="load__icon">
+                      <path fill="#4ce1b6" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+                    </svg>
+                  </div>
+                </div>
+              )
+              }
+              {!this.state.reportModalLoading
+              && (
+                <>
+                  <ModalBody>
+                    <Row className="search-input">
+                      <Col lg={12} md={12} sm={12} xs={12}>
+                        <FormGroup>
+                          {renderSelectField({
+                            input: {
+                              onChange: (e) => { this.handleSelect('reportType', e); },
+                              isMulti: false,
+                              name: 'reportType',
+                              value: this.state.reportType,
+                              clearable: true,
+                            },
+                            placeholder: 'علت گزارش',
+                            options: reportTypes,
+                            name: 'select',
+                            type: 'text',
+                          })}
+                        </FormGroup>
+                      </Col>
+                      {!this.isItemValid('reportType')
+                      && (
+                        <Col lg={12} md={12} sm={12} xs={12} className="text-danger">
+                          <p>انتخاب علت گزارش الزامی است</p>
+                        </Col>
+                      )}
+                    </Row>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      color="primary"
+                      onClick={() => { this.handleSaveReport(); }}
+                      disabled={!this.isItemValid('reportType')}
+                    >
+                      ذخیره درخواست
+                    </Button>
+                    <Button
+                      color="secondary"
+                      onClick={() => { this.handleDismissModal('reportModal'); }}
+                    >
+                      بازگشت
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </Modal>
             <h4 className="text-center">{this.state.adsInfo.title}</h4>
             <Row className="ads-detail-height">
               <Col lg={8} md={7} sm={12} xs={12}>
@@ -287,6 +432,18 @@ class AdsDetailForm extends PureComponent {
                       <Col md={4} className="ads-col-btn">
                         <Button className="btn-ads btn-success" onClick={this.handleEmail}>ایمیل</Button>
                       </Col>
+                    </Row>
+                    <Row>
+                      <Col />
+                      <Col md={4} className="ads-col-btn">
+                        <Button
+                          className="btn-ads btn-success"
+                          onClick={() => { this.handleToggleModal('reportModal'); }}
+                        >
+                          گزارش تخلف
+                        </Button>
+                      </Col>
+                      <Col />
                     </Row>
                   </CardBody>
                 </Card>
